f9957ce0e96ba9287736387c9fae94b1a81bd103
Ricky <destinypatrick4772@gmail.com>
2025-06-20 16:38:47 +0100

    Add files via upload

---
+++ Java script assignment on if statements and for loop/index.js
@@ -0,0 +1,59 @@
+// Score Checker
+
+const a = 70
+if (a >= 70) {
+    console.log("Excellent")
+} else if (a >= 60 & a <= 69) {
+    console.log("Very Good")
+} else if (a >= 50 && a <= 59) {
+    console.log("Good")
+}
+else if (a >= 40 && a <= 49) {
+    console.log("Pass")
+}
+else if (a >= 0 && a <= 39) {
+    console.log("Fail")
+}
+
+// array
+const js1 = ["Delight", "Bright", "Glory", "Eze", "Praise"]
+const js2 = ["Elvis", "Emilia", "Paul", "Stella", "Jadon"]
+const js3 = ["Chisom", "Saviour", "Kelvin", "Gold", "Jenny"]
+const ss1 = ["Salvation", "Wisdom", "Chizi", "Favour", "Lizzy"]
+const ss2 = ["Francisca", "Juanita", "Joshua", "Jerry", "Ella"]
+const ss3 = ["Emerald", "Destiny", "Nancy", "Ibiso", "Faith"]
+
+// loops
+
+// JS1 LOOP
+for(let b = 0; b < js1.length; b++){
+    console.log(js1[b])
+}
+
+// JS2 LOOP
+for(let b = 0; b < js2.length; b++){
+    console.log(js2[b])
+}
+
+// JS3 LOOP
+for(let b = 0; b < js3.length; b++){
+    console.log(js3[b])
+}
+
+// SS1 LOOP
+for(let b = 0; b < ss1.length; b++){
+    console.log(ss1[b])
+}
+
+// SS2 LOOP
+for(let b = 0; b < ss2.length; b++){
+    console.log(ss2[b])
+}
+
+// SS3 LOOP
+for(let b = 0; b < ss3.length; b++){
+    console.log(ss3[b])
+}
+
+
+
